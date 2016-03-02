23a094c7f66aa24fff1a7fba9f66120f0f4b1e6a
DeltaQuad <deltaquadwiki@gmail.com>
2016-03-01 23:56:27 -0500

    start

---
+++ IPBE.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+import globalfunc as globe
+import sys
+import platform
+import localconfig
+if platform.system() == "Windows":
+        sys.path.append(localconfig.winpath)
+else:sys.path.append(localconfig.linuxpath)
+import wikipedia
+import json
+def main():
+    globe.getUserList()
+if not globe.startAllowed():
+        print "Fatal - System Access Denied."
+        sys.exit(1)
+        print "System Alert - Program Still running."
+main()
