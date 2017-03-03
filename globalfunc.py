b4a70f068222de3eadbc3d725270c10b4a3a5199
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2017-03-03 00:08:23 -0500

    add talkpage post

--- globalfunc.py
+++ globalfunc.py
@@ -5,6 +5,7 @@ if platform.system() == "Windows":
         sys.path.append(localconfig.winpath)
 else:sys.path.append(localconfig.linuxpath)
 import pywikibot
+import datetime
 from pywikibot.data import api
 
 useWiki= pywikibot.Site('en','wikipedia')
@@ -49,8 +50,33 @@ def query(user):
         #print log
         for event in log:
                 if event["params"]["oldgroups"] == '': event["params"]["oldgroups"]="None"
-                if not "ipblock-exempt" in event["params"]["oldgroups"] and "ipblock-exempt" in event["params"]["newgroups"]:return event["timestamp"]+ " [[User:" + event["user"] + "|" + event["user"] + "]] ([[User talk:" + event["user"] + "|talk]] | [[Special:Contributions/" + event["user"] + "|contribs]] | [[Special:Block/" + event["user"] + "|block]])" + " changed rights for [[" +event["title"] + "]] from " + ','.join(event["params"]["oldgroups"]) + " to " + ','.join(event["params"]["newgroups"]) + " per '" + event["comment"] + "'"
+                if not "ipblock-exempt" in event["params"]["oldgroups"] and "ipblock-exempt" in event["params"]["newgroups"]:
+			addToTalk(event["timestamp"],event["user"],event["comment"])
+			return event["timestamp"]+ " [[User:" + event["user"] + "|" + event["user"] + "]] ([[User talk:" + event["user"] + "|talk]] | [[Special:Contributions/" + event["user"] + "|contribs]] | [[Special:Block/" + event["user"] + "|block]])" + " changed rights for [[" +event["title"] + "]] from " + ','.join(event["params"]["oldgroups"]) + " to " + ','.join(event["params"]["newgroups"]) + " per '" + event["comment"] + "'"
                 #print "Event: "+event["timestamp"]+ " " + event["user"] + " changed userrights for " +event["title"] + " from " + event["rights"]["old"] + " to " + event["rights"]["new"] + " because " + event["comment"]
+def sendToTalk(timestamp,username,reason):
+	pagename = localconfig.listlocation
+        page = pywikibot.Page(site, pagename)
+        pagetxt = page.get()
+	if username in pagetxt:return
+	now = datetime.datetime.now()
+	dateofchange = event["timestamp"].split("Z")[0]
+	year = dateofchange.split("-")[0]
+	if year < now.year:return
+	month = dateofchange.split("-")[1].replace("0","")
+	monthword = now.strftime("%B")
+	if not "=="+year+"==" in pagetxt or not "== "+year+" ==" in pagetxt:
+		pagetxt += "=="+year+"=="
+	try:pageyear = pagetxt.split("=="+year+"==")[1]
+	except:pageyear = pagetxt.split("== "+year+" ==")[1]
+	if not monthword in pageyear:
+		pagetxt += "==="+monthword+"==="
+	if year == now.year:
+		if month == now.month:
+			pagetxt += "\n*{{UserIPBE|" + username+"}} - "+reason+" ~~~~"
+	summary = localconfig.summary
+        page.put(pagetxt, comment=summary + " - Adding [[User:"+username+"|"+username+"]]")
+				
 def sendPage(text, txtformat):
     #print text
     summary = localconfig.summary
