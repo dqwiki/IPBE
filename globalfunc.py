8914ffecefc15085f5b35070ecf480fba3f69869
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2017-03-03 01:15:51 -0500

    fix

--- globalfunc.py
+++ globalfunc.py
@@ -60,7 +60,8 @@ def sendToTalk(timestamp,username,reason,admin):
 	now = datetime.datetime.now()
 	dateofchange = timestamp.split("Z")[0]
 	year = dateofchange.split("-")[0]
-	if username in pagetxt.split(year)[1]:return
+	try:if username in pagetxt.split("=="+year+"==")[1]:return
+	except:if username in pagetxt.split("== "+year+" ==")[1]:return
 	if year != str(now.year):return
 	month = dateofchange.split("-")[1].replace("0","")
 	monthword = now.strftime("%B")
